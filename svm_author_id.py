3767f6c79c83f0f8c22a57cf2004704643688dc0
hmallajosyula <harshazkool@gmail.com>
2016-01-05 19:57:40 -0800

    Added SVM Mini Project

---
+++ svm_author_id.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("/Users/harshamallajosyula/documents/SlideRule-DataScience/ud120-projects/tools/")
+from email_preprocess import preprocess
+import numpy as np
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+
+
+#########################################################
+### your code goes here ###
+from sklearn import svm
+clf = svm.SVC(kernel='rbf', C=10000)
+
+#reducing the size of the feature vectors
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
+
+print "Prediction for 10th test set is %d" %pred[10]
+print "Prediction for 26th test set is %d" %pred[26]
+print "Prediction for 50th test set is %d" %pred[50]
+
+#predictions in the Chris Class
+print " Total Predictions for Chris Class is %d" %sum(pred)
+
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred,labels_test)
+print "Accuracy of Linear SVM is %f" %acc
+#########################################################
+
+
